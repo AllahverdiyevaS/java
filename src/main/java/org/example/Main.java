4331949ae7cee161d773ce6eaf1a6ab4d3626d7c
homework <allahverdiyevashafayat@outlook.com>
2023-07-18 22:58:47 +0200

    homework

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+       float myFloat = 4.7333436f;
+       double myDouble = 4.355453532;
+       long myLong = 12121;
+
+        System.out.println("char: " + myChar);
+        System.out.println("int: " + myInt);
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("long: " + myLong);
+
+        Character charObject = 'G';
+        Integer intObject = 89;
+        Byte byteObject = 4;
+        Short shortObject = 56;
+        Float floatObject = 4.7333436f;
+        Double doubleObject = 4.355453532;
+        Long longObject = 12121L;
+        System.out.println("Character: " + charObject);
+        System.out.println("Integer: " + intObject);
+        System.out.println("Byte: " + byteObject);
+        System.out.println("Short: " + shortObject);
+        System.out.println("Float: " + floatObject);
+        System.out.println("Double: " + doubleObject);
+        System.out.println("Long: " + longObject);
+
+
+
+
+
+int value = 345;
+        int a1 = value / 100;
+        int a2 = (value %100)/10;
+        int a3 = value % 10;
+        System.out.println(value);
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+
+
+    }
+}
